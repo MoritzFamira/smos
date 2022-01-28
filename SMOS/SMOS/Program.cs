89448bbccf78044bb64348d5ebf7be5606820559
Moritz <famiramoritz@gmail.com>
2022-01-28 14:37:49 +0100

    database creation now up to spec with whiteboard (28.1.22)

--- SMOS/SMOS/Program.cs
+++ SMOS/SMOS/Program.cs
@@ -44,20 +44,34 @@ try
     {
         //TODO Check if the database is there
         string schemaAndDatabaseTest = @"create database if not exists mos;
-        use mos;
-        set foreign_key_checks = 0;
-        create table if not exists p_products(
-            p_id int primary key,
-            p_name varchar(255),
-            p_price int,
-            p_mt_type int,
-            foreign key (p_mt_type) references mt_merchtypes (mt_id)
-        );
-        create table if not exists mt_merchtypes (
-            mt_id int primary key,
-            mt_name varchar(255)
-        );
-        set foreign_key_checks = 1;";
+use mos;
+set foreign_key_checks = 0;
+create table if not exists p_products(
+    p_id int primary key not null auto_increment,
+    p_name varchar(255),
+    p_price int,
+    p_mt_type int
+);
+create table if not exists u_users(
+    u_id int primary key not null auto_increment,
+    u_name varchar(255) not null,
+    u_isadmin bool,
+    u_dateofcreation datetime
+);
+create table if not exists s_submissions(
+    s_id int primary key not null auto_increment,
+    s_u_artist int,
+    s_votes int,
+    s_date datetime,
+    #TODO
+    foreign key (s_u_artist) references u_users (u_id)
+);
+create table if not exists t_tshirts(
+  t_p_product int primary key not null auto_increment,
+  t_size varchar(16),
+  t_color varchar(16)
+);
+set foreign_key_checks = 1;";
         var cmd = new MySqlCommand(schemaAndDatabaseTest, dbCon.Connection);
         Console.WriteLine("Creating Database");
         cmd.ExecuteReader();
--- SMOS/SMOS/SqlFiles/Initiate MOS Database.sql
+++ SMOS/SMOS/SqlFiles/Initiate MOS Database.sql
@@ -5,12 +5,7 @@ create table if not exists p_products(
     p_id int primary key not null auto_increment,
     p_name varchar(255),
     p_price int,
-    p_mt_type int,
-    foreign key (p_mt_type) references mt_merchtypes (mt_id)
-);
-create table if not exists mt_merchtypes (
-    mt_id int primary key not null auto_increment,
-    mt_name varchar(255)
+    p_mt_type int
 );
 create table if not exists u_users(
     u_id int primary key not null auto_increment,
@@ -21,7 +16,14 @@ create table if not exists u_users(
 create table if not exists s_submissions(
     s_id int primary key not null auto_increment,
     s_u_artist int,
+    s_votes int,
+    s_date datetime,
     #TODO
     foreign key (s_u_artist) references u_users (u_id)
+);
+create table if not exists t_tshirts(
+  t_p_product int primary key not null auto_increment,
+  t_size varchar(16),
+  t_color varchar(16)
 );
 set foreign_key_checks = 1;
