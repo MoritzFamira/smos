8ad693012306570b0c8fb2ee1b610a130e52955e
Moritz <famiramoritz@gmail.com>
2022-02-05 17:30:08 +0100

    reorganized controllers

---
+++ SMOS/SMOS/Controllers/AddProductController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using SMOS.DataBase;
+
+namespace SMOS.Controllers;
+
+[ApiController]
+[Route("AddProduct")]
+public class AddProductController : ControllerBase
+{
+    [HttpPost(Name = "AddProduct")]
+    public void Post()
+    {
+        var dbCon = DBConnection.Instance();
+        //this is needed to reset the connection
+        dbCon.Reset();
+        try
+        {
+            if (dbCon.IsConnect())
+            {
+                
+                string addProduct = @"use mos; 
+insert into p_products (p_id, p_name, p_price)
+VALUE (null,@name,@price);";
+                var cmd = new MySqlCommand(addProduct,dbCon.Connection);
+                
+                var name = new MySqlParameter("name", "TEST");
+                var price = new MySqlParameter("price", 1);
+
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@price", price);
+                Console.WriteLine("Adding Product");
+                cmd.ExecuteNonQuery();
+                dbCon.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Cannot connect to Database!\n{e}");
+        }
+    }
+    
+}
