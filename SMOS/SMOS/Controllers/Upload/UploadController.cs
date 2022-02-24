92e6128e3c53585b43bedcdf8293c6b4cd16877f
Moritz <famiramoritz@gmail.com>
2022-02-24 11:38:27 +0100

    added UploadController.cs

--- SMOS/SMOS/Controllers/Upload/UploadController.cs
+++ SMOS/SMOS/Controllers/Upload/UploadController.cs
@@ -2,30 +2,27 @@
 
 namespace SMOS.Controllers.Upload;
 
-
-public class UploadController:Controller
+[ApiController]
+[Route("api/Upload")]
+public class UploadController : ControllerBase
 {
-    [HttpPost]  
-    public HttpResponseMessage Upload( file)  
-    {  
-        if (file != null && file.ContentLength > 0)  
-            try 
-            {  //Server.MapPath takes the absolute path of folder 'Uploads'
-                //TODO 
-                string path = Path.Combine(Directory.GetCurrentDirectory(),  
-                    Path.GetFileName(file.FileName));  
-                //Save file using Path+fileName take from above string
-                file.SaveAs(path);  
-                ViewBag.Message = "File uploaded successfully";  
-            }  
-            catch (Exception ex)  
-            {  
-                ViewBag.Message = "ERROR:" + ex.Message.ToString();  
-            }  
-        else 
-        {  
-            ViewBag.Message = "You have not specified a file.";  
-        }  
+    [HttpPost(Name = "Upload")]
+    public IActionResult UploadFile(IFormFile file)
+    {
+        //TODO way of finding path to upload folder
+        /*var ctx = HttpContext.;
+        var root = ctx.Server.MapPath();*/
+        //var provider = new MultipartFormDataContent("~/Pages");
+        try
+        {
+            Console.WriteLine("Datei: "+file.FileName);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return Ok();
     }
-    
 }
