e901e31bd4e19c809a31ddaec9d63533359f3523
Julian <MEY20423@spengergasse.at>
2022-04-22 14:42:00 +0200

    cup update

--- SMOS/SMOS/Controllers/Gets/CupController.cs
+++ SMOS/SMOS/Controllers/Gets/CupController.cs
@@ -20,7 +20,7 @@ public class CupController : ControllerBase
         {
             if (dbCon.IsConnect())
             {
-                string getProducts = @"use mos; select c_id,c_name,c_price,c_size,c_color,c_countryofmanufacturer
+                string getProducts = @"use mos; select c_id,c_name,c_price,c_color,c_countryofmanufacturer
                  from c_cups";
                 var cmd = new MySqlCommand(getProducts, dbCon.Connection);
                 Console.WriteLine("Getting Cups");
