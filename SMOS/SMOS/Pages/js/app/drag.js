8e3de90ee4ddc3a245fe147e0b7bb8dbbdaa3ea9
Julian <Julian.meyringer@hotmail.com>
2022-06-05 21:14:21 +0200

    Add Drag and Drop

---
+++ SMOS/SMOS/Pages/js/app/drag.js
@@ -0,0 +1,38 @@
+//selecting all required elements
+const dropArea = document.querySelector(".drag-area"),
+    dragText = dropArea.querySelector("header");
+let file; //this is a global variable and we'll use it inside multiple functions
+//If user Drag File Over DropArea
+dropArea.addEventListener("dragover", (event)=>{
+    event.preventDefault(); //preventing from default behaviour
+    dropArea.classList.add("active");
+    dragText.textContent = "Release to Upload File";
+});
+//If user leave dragged File from DropArea
+dropArea.addEventListener("dragleave", ()=>{
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+});
+//If user drop File on DropArea
+let dropfile = "";
+dropArea.addEventListener("drop", (event)=>{
+    event.preventDefault(); //preventing from default behaviour
+    //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+    dropfile = event.dataTransfer.files[0];
+    dragText.textContent = ""+dropfile.name;
+    $("#file").remove();
+});
+async function dragfetch(){
+    if(dropfile != ""){
+        let formData = new FormData($("#upload-form")[0])
+        formData.append("file", dropfile)
+        formData.append("artist", parseInt(localStorage.userid))
+        let response = await fetch('../../api/Upload', {
+            method: 'POST',
+            body: formData,
+            enctype: "multipart/form-data"
+        });
+        formData.reset();
+        dropfile = "";     
+    }
+}
